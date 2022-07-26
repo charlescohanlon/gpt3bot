05e746b62e41d06ac07801d710350630d90c2999
Charles O'Hanlon <charlescohanlon@gmail.com>
2022-07-26 15:52:52 -0700

    update gitignore

--- GPT3Bot.js
+++ GPT3Bot.js
@@ -1,5 +1,18 @@
-const { WebClient } = require("@slack/web-api");
+const { App } = require("@slack/bolt");
 require("dotenv").config();
 
-console.log(process.env.SLACK_TOKEN)
-// const web = new WebClient(process.env.SLACK_TOKEN);
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+app.message("hello", async ({ message, say }) => {
+  await say(`Hello there <@${message.user}>`);
+});
+
+
+(async () => {
+  await app.start(3000);
+})();
