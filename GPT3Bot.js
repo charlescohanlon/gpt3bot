ef2665301db497c00856190143056f2e5d7c3995
Charles O'Hanlon <charlescohanlon@gmail.com>
2022-07-26 20:35:04 -0700

    implement state machine approach

--- GPT3Bot.js
+++ GPT3Bot.js
@@ -2,17 +2,54 @@ const { App } = require("@slack/bolt");
 require("dotenv").config();
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	socketMode: true,
+	appToken: process.env.SLACK_APP_TOKEN,
 });
 
-app.message("hello", async ({ message, say }) => {
-  await say(`Hello there <@${message.user}>`);
+
+
+const States = {
+	STARTED: "STARTED",
+	NOTSTARTED: "NOTSTARTED",
+	RECIPIENTCHOSEN: "RECIPIENTCHOSEN",
+}
+
+let state = States.NOTSTARTED;
+let recipient;
+app.message(async ({ message: { text }, say }) => {
+
+	if (state === States.NOTSTARTED && text.match(/Hello (gpt3|gpt-3)/i)) {
+		await say("Hello, which AI would you like to talk to (ada, babbage, curie, or davinci)?")
+		state = States.STARTED;
+		return;
+	}
+
+	if (state === States.STARTED) {
+		await chooseRecipient(text, say);
+		if (recipient) {
+			state = States.RECIPIENTCHOSEN;
+			say(`You are now talking to ${recipient}.`);
+		}
+		return;
+	}
+
+	if (state === States.RECIPIENTCHOSEN) {
+		return;
+	}
+
 });
+
+async function chooseRecipient(text, say) {
+	if (text.match(/(ada)/)) recipient = "ada";
+	else if (text.match(/(babbage)/)) recipient = "babbage";
+	else if (text.match(/(curie)/)) recipient = "curie";
+	else if (text.match(/(davinci)/)) recipient = "davinci";
+	else await say("I'm afraid I don't understand. Who would you like to talk to (ada, babbage, curie, or davinci)?");
+}
 
 
 (async () => {
-  await app.start(3000);
+	await app.start(3000);
 })();
